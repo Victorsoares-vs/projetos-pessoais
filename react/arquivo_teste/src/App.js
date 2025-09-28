54f2b27fbeae0a14ea1b9093bca57e8c9bac8585
Victorsoares-vs <victortrexbk@gmail.com>
2025-09-28 14:21:39 -0300

    Install base file
    Instalação de arquivo base para estudos

---
+++ react/arquivo_teste/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello world!</h1>
+        <p>Esse é meu primeiro teste com React!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
